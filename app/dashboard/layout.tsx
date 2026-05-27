0b62f2558c9f586245ffc788c51c7de4bd4a2ede
Clark Daryll R. Brucal <ajuaaw@gmail.com>
2026-05-27 15:31:46 +0800

    COmmit

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import Sidebar from "./Sidebar";
+import { SidebarWrapper } from "./SidebarWrapper";
 
 export default async function DashboardLayout({
   children,
@@ -12,17 +12,12 @@ export default async function DashboardLayout({
 
   return (
     <div style={{ display: "flex", minHeight: "100vh", background: "#0a0a0a" }}>
-      <Sidebar
+      <SidebarWrapper
         firstName={user.firstName}
         lastName={user.lastName}
         email={user.emailAddresses[0]?.emailAddress}
       />
-      <main style={{
-        marginLeft: 240,
-        flex: 1,
-        padding: 32,
-        color: "white",
-      }}>
+      <main style={{ marginLeft: 240, flex: 1, padding: 32, color: "white" }}>
         {children}
       </main>
     </div>
